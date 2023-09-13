4826651b4ec78d8164efc6a2811f37e72aff71fa
snehalp0104 <84397125+snehalp0104@users.noreply.github.com>
2023-09-14 01:02:00 +0530

    Added solution - LeetHub

---
+++ M-Coloring Problem - GFG/mcoloring-problem.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+public:
+    // Function to determine if graph can be coloured with at most M colours such
+    // that no two adjacent vertices of graph are coloured with same colour.
+    bool ispossible(int node,int col,bool graph[101][101],int n,vector<int>&color)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(i!=node and graph[i][node]==1 and color[i]==col) return false;
+        }
+        return true;
+    }
+    bool solve(int node,int m,int n,bool graph[101][101],vector<int>&color)
+    {
+        if(node==n) return true;
+        for(int i=1;i<=m;i++)
+        {
+            if(ispossible(node,i,graph,n,color))
+            {
+                color[node]=i;
+                if(solve(node+1,m,n,graph,color)) return true;
+                color[node]=0;
+            }
+        }
+        return false;
+    }
+    
+    
+    bool graphColoring(bool graph[101][101], int m, int n) {
+        // your code here
+        vector<int> color(n,0);
+        if(solve(0,m,n,graph,color)==true) return true;
+        return false;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, e;
+        cin >> n >> m >> e;
+        int i;
+        bool graph[101][101];
+        for (i = 0; i < n; i++) {
+            memset(graph[i], 0, sizeof(graph[i]));
+        }
+        for (i = 0; i < e; i++) {
+            int a, b;
+            cin >> a >> b;
+            graph[a - 1][b - 1] = 1;
+            graph[b - 1][a - 1] = 1;
+        }
+        Solution ob;
+        cout << ob.graphColoring(graph, m, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
